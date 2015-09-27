c8bf357b49d6f4b2fe394867a22fdb3d2893c5c6
jeichenhofer <eichenhofer@wisc.edu>
2015-09-26 23:14:48 -0500

    Implemented very simple web interface.

--- src/web/web.py
+++ src/web/web.py
@@ -2,22 +2,27 @@ from flask import Flask
 from flask import render_template
 from flask import redirect
 from flask import url_for
+from src.libs.web_interface import get_user_by_handle
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return redirect(url_for('lookup_user', user_handle='@sampleHandle'))
+    return redirect(url_for('lookup_user', user_handle='sampleHandle'))
 
 @app.route('/u/<user_handle>')
 def lookup_user(user_handle):
 
-    user_name = 'SAMPLE NAME'
-    user_score = 'SAMPLE SCORE (in units of evil)'
-    user_degree = 'SAMPLE DEGREE'
+    data = get_user_by_handle(user_handle)
+    user_name = data['name']
+    user_score = data['score']
+    user_route = data['route']
+    user_degree = len(user_route)
+    user_kingpin = user_route[-1]
 
-    return render_template('user_data_report.html', handle=user_handle, name=user_name,
-                           score=user_score, degree=user_degree)
+    return render_template('user_data_report.html', handle=user_handle,
+                           name=user_name, score=user_score, degree=user_degree,
+                           route=user_route, kingpin=user_kingpin)
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
